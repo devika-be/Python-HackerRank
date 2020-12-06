a565dc6095020ba604863e219e78444f40f7f802
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2020-12-06 11:21:19 +0530

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,25 +1,5 @@
-QUS- 
-Here is a sample line of code that can be executed in Python:
+#Problem link : https://www.hackerrank.com/challenges/py-hello-world/problem
 
+#ANS-
 print("Hello, World!")
-You can just as easily store a string as a variable and then print it to stdout:
 
-my_string = "Hello, World!"
-print(my_string)
-The above code will print Hello, World! on your screen. Try it yourself in the editor below!
-
-Input Format
-
-You do not need to read any input in this challenge.
-
-Output Format
-
-Print Hello, World! to stdout.
-
-Sample Output 0
-
-Hello, World!
-
-
-ANS-
-print("Hello, World!")
