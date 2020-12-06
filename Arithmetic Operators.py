29e4de8a4768510524319802d997a1e8bf5108dd
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2020-12-06 11:26:13 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,9 @@
+# Problem link :https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+# ANS:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
