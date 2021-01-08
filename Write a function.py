b35bf7c0e657163d3d44f27022eedfa5b6f5503c
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-01-08 11:52:33 +0530

    Create Write a function.py

---
+++ Write a function.py
@@ -0,0 +1,12 @@
+# Problem link :https://www.hackerrank.com/challenges/write-a-function/problem
+
+# Ans:
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap = True
+    return leap
