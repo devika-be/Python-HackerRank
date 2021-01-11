8f8e61f0854ced2971fdf3f016e0927e05779209
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-01-11 15:00:44 +0530

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,6 @@
+#Problem Link : https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+#Ans :
+if __name__ == '__main__':
+    x,y,z,n = (int(input()) for _ in range(4))
+print([[a,b,c] for a in range(x+1)for b in range(y+1)for c in range(z+1) if a+b+c !=n])
