aa73dee20f6a1b9861fd7ee4c78d69facc768e5f
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-01-11 14:58:19 +0530

    Create Print Function.py

---
+++ Print Function.py
@@ -0,0 +1,7 @@
+#Problem Link : https://www.hackerrank.com/challenges/python-print/problem 
+
+#Ans :
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
