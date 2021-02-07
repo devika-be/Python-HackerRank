314cc25ca6bad558201c97d0ccb38cd043b8c11b
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-02-07 20:38:43 +0530

    Create What's Your Name?.py

---
+++ What's Your Name?.py
@@ -0,0 +1,6 @@
+#Problem Link : https://www.hackerrank.com/challenges/whats-your-name/problem
+
+#Ans :
+
+def print_full_name(a, b):
+    print("Hello " + a + " " + b +"! You just delved into python.")
