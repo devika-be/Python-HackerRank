cb5c72fbe9172c46fe7311c6aaee62e3a7fc8377
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-02-13 16:26:34 +0530

    Create Find a string.py

---
+++ Find a string.py
@@ -0,0 +1,10 @@
+#Problem Link : https://www.hackerrank.com/challenges/find-a-string/problem
+
+#Ans :
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if string[i:len(sub_string)+i] == sub_string:
+            count += 1
+    return count
