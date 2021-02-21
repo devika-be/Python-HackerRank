cff5e8200dc2bd5f1fe6d9695325e8c8f9f3a172
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-02-21 18:09:19 +0530

    Create String Validators.py

---
+++ String Validators.py
@@ -0,0 +1,8 @@
+#Problem Link : https://www.hackerrank.com/challenges/string-validators/problem
+
+#Ans :
+
+if __name__ == '__main__':
+    s = input()
+    for method in [str.isalnum, str.isalpha, str.isdigit, str.islower, str.isupper]:
+        print(any(method(i) for i in s))
