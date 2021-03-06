be58cff772f2ef301add8e8f8858863004ba62a1
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-03-06 11:24:16 +0530

    Create Designer Door Mat.py

---
+++ Designer Door Mat.py
@@ -0,0 +1,7 @@
+#Problem Link : https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+#Ans :
+
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i +1)).center(m, '-')for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME' .center(m , '-')] + pattern[::-1]))
