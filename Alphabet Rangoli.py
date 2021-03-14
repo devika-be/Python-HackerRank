8b956b69fea54d21bb5ebf7ab219a600dd2febf8
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-03-14 22:07:47 +0530

    Create Alphabet Rangoli.py

---
+++ Alphabet Rangoli.py
@@ -0,0 +1,13 @@
+#Problem Link : https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+
+#Ans :
+
+import string
+def print_rangoli(size):
+    a = string.ascii_lowercase
+    r = []
+    for i in range(size):
+        s = "-".join(a[i:size])
+        r.append((s[::-1]+s[1:]).center(4*size-3, "-"))
+
+    print('\n'.join(r[:0:-1]+r))
