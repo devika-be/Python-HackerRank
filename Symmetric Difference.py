c15803091e6303b28e87e6d40f369a87fc7c3a1c
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-04-10 20:21:58 +0530

    Create Symmetric Difference.py

---
+++ Symmetric Difference.py
@@ -0,0 +1,14 @@
+#Problem Link : https://www.hackerrank.com/challenges/symmetric-difference/problem
+
+#Ans :
+
+input()
+a = set(map(int, input().split()))
+input()
+b = set(map(int, input().split()))
+a_d = a.difference(b)
+b_d = b.difference(a)
+for i in b_d:
+    a_d.add(i)
+for i in sorted(a_d):
+    print(i)
